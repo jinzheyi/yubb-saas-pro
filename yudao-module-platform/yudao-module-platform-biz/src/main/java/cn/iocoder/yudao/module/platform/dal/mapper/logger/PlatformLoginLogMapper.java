d2116d9d5788866527348a62693733a33857d3e3
zhusy <jin_zheyicn@qq.com>
2022-07-07 18:10:37 +0800

    初始化平台管理日志处理接口

--- yudao-module-platform/yudao-module-platform-biz/src/main/java/cn/iocoder/yudao/module/platform/dal/mapper/logger/PlatformLoginLogMapper.java
+++ yudao-module-platform/yudao-module-platform-biz/src/main/java/cn/iocoder/yudao/module/platform/dal/mapper/logger/PlatformLoginLogMapper.java
@@ -2,7 +2,7 @@ package cn.iocoder.yudao.module.platform.dal.mapper.logger;
 
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
-import cn.iocoder.yudao.framework.mybatis.core.query.QueryWrapperX;
+import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.iocoder.yudao.module.platform.controller.center.logger.vo.loginlog.LoginLogExportReqVO;
 import cn.iocoder.yudao.module.platform.controller.center.logger.vo.loginlog.LoginLogPageReqVO;
 import cn.iocoder.yudao.module.platform.dal.dataobject.logger.LoginLogDO;
@@ -15,30 +15,30 @@ import java.util.List;
 public interface PlatformLoginLogMapper extends BaseMapperX<LoginLogDO> {
 
     default PageResult<LoginLogDO> selectPage(LoginLogPageReqVO reqVO) {
-        QueryWrapperX<LoginLogDO> query = new QueryWrapperX<LoginLogDO>()
-                .likeIfPresent("user_ip", reqVO.getUserIp())
-                .likeIfPresent("username", reqVO.getUsername())
-                .betweenIfPresent("create_time", reqVO.getBeginTime(), reqVO.getEndTime());
+        LambdaQueryWrapperX<LoginLogDO> query = new LambdaQueryWrapperX<LoginLogDO>()
+                .likeIfPresent(LoginLogDO::getUserIp, reqVO.getUserIp())
+                .likeIfPresent(LoginLogDO::getUsername, reqVO.getUsername())
+                .betweenIfPresent(LoginLogDO::getCreateTime, reqVO.getBeginTime(), reqVO.getEndTime());
         if (Boolean.TRUE.equals(reqVO.getStatus())) {
-            query.eq("result", PlatformLoginResultEnum.SUCCESS.getResult());
+            query.eq(LoginLogDO::getResult, PlatformLoginResultEnum.SUCCESS.getResult());
         } else if (Boolean.FALSE.equals(reqVO.getStatus())) {
-            query.gt("result", PlatformLoginResultEnum.SUCCESS.getResult());
+            query.gt(LoginLogDO::getResult, PlatformLoginResultEnum.SUCCESS.getResult());
         }
-        query.orderByDesc("id"); // 降序
+        query.orderByDesc(LoginLogDO::getId); // 降序
         return selectPage(reqVO, query);
     }
 
     default List<LoginLogDO> selectList(LoginLogExportReqVO reqVO) {
-        QueryWrapperX<LoginLogDO> query = new QueryWrapperX<LoginLogDO>()
-                .likeIfPresent("user_ip", reqVO.getUserIp())
-                .likeIfPresent("username", reqVO.getUsername())
-                .betweenIfPresent("create_time", reqVO.getBeginTime(), reqVO.getEndTime());
+        LambdaQueryWrapperX<LoginLogDO> query = new LambdaQueryWrapperX<LoginLogDO>()
+                .likeIfPresent(LoginLogDO::getUserIp, reqVO.getUserIp())
+                .likeIfPresent(LoginLogDO::getUsername, reqVO.getUsername())
+                .betweenIfPresent(LoginLogDO::getCreateTime, reqVO.getBeginTime(), reqVO.getEndTime());
         if (Boolean.TRUE.equals(reqVO.getStatus())) {
-            query.eq("result", PlatformLoginResultEnum.SUCCESS.getResult());
+            query.eq(LoginLogDO::getResult, PlatformLoginResultEnum.SUCCESS.getResult());
         } else if (Boolean.FALSE.equals(reqVO.getStatus())) {
-            query.gt("result", PlatformLoginResultEnum.SUCCESS.getResult());
+            query.gt(LoginLogDO::getResult, PlatformLoginResultEnum.SUCCESS.getResult());
         }
-        query.orderByDesc("id"); // 降序
+        query.orderByDesc(LoginLogDO::getId); // 降序
         return selectList(query);
     }
 
