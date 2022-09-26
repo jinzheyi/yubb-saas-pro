6bbb722f12a6c26168c98169a7cd69cda55c3c95
朱述勇 <jin_zheyicn@qq.com>
2022-09-26 19:58:04 +0800

    Revert "生成插件商城代码"
    This reverts commit 78e5a29c31057730532467aec4c6776ceeb5749f.

--- yudao-module-platform/yudao-module-platform-biz/src/main/java/cn/iocoder/yudao/module/platform/controller/center/plug/vo/goods/PlugGoodsExportReqVO.java
+++ yudao-module-platform/yudao-module-platform-biz/src/main/java/cn/iocoder/yudao/module/platform/controller/center/plug/vo/goods/PlugGoodsExportReqVO.java
@@ -1,12 +1,10 @@
 package cn.iocoder.yudao.module.platform.controller.center.plug.vo.goods;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
+import lombok.*;
+import java.util.*;
+import io.swagger.annotations.*;
+import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import org.springframework.format.annotation.DateTimeFormat;
-
-import java.math.BigDecimal;
-import java.util.Date;
 
 import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 
@@ -21,9 +19,11 @@ public class PlugGoodsExportReqVO {
     private String appSn;
 
     @ApiModelProperty(value = "原价", example = "0")
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private BigDecimal[] appPrice;
 
     @ApiModelProperty(value = "售价", example = "0")
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private BigDecimal[] payPrice;
 
     @ApiModelProperty(value = "数量", example = "1")
@@ -32,7 +32,7 @@ public class PlugGoodsExportReqVO {
     @ApiModelProperty(value = "状态 上下架")
     private Integer appStatus;
 
-    @ApiModelProperty(value = "创建时间", example = "[2022-07-01 00:00:00,2022-07-01 23:59:59]")
+    @ApiModelProperty(value = "创建时间")
     @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private Date[] createTime;
 
--- yudao-module-platform/yudao-module-platform-biz/src/main/java/cn/iocoder/yudao/module/platform/dal/mysql/plug/PlugOrderItemMapper.java
+++ yudao-module-platform/yudao-module-platform-biz/src/main/java/cn/iocoder/yudao/module/platform/dal/mysql/plug/PlugOrderItemMapper.java
@@ -1,8 +1,13 @@
 package cn.iocoder.yudao.module.platform.dal.mysql.plug;
 
+import java.util.*;
+
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
-import cn.iocoder.yudao.module.platform.dal.dataobject.plug.PlugOrderItemDO;
+import cn.iocoder.yudao.module.plug.dal.dataobject.orderitem.PlugOrderItemDO;
 import org.apache.ibatis.annotations.Mapper;
+import cn.iocoder.yudao.module.plug.controller.admin.orderitem.vo.*;
 
 /**
  * 订单项 Mapper
@@ -11,5 +16,41 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface PlugOrderItemMapper extends BaseMapperX<PlugOrderItemDO> {
+
+    default PageResult<PlugOrderItemDO> selectPage(PlugOrderItemPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<PlugOrderItemDO>()
+                .eqIfPresent(PlugOrderItemDO::getOrderId, reqVO.getOrderId())
+                .eqIfPresent(PlugOrderItemDO::getAppId, reqVO.getAppId())
+                .eqIfPresent(PlugOrderItemDO::getAppPic, reqVO.getAppPic())
+                .likeIfPresent(PlugOrderItemDO::getAppName, reqVO.getAppName())
+                .eqIfPresent(PlugOrderItemDO::getAppSn, reqVO.getAppSn())
+                .eqIfPresent(PlugOrderItemDO::getAppPrice, reqVO.getAppPrice())
+                .eqIfPresent(PlugOrderItemDO::getPayNum, reqVO.getPayNum())
+                .eqIfPresent(PlugOrderItemDO::getPromotionAmount, reqVO.getPromotionAmount())
+                .eqIfPresent(PlugOrderItemDO::getDiscountAmount, reqVO.getDiscountAmount())
+                .eqIfPresent(PlugOrderItemDO::getGiftIntegration, reqVO.getGiftIntegration())
+                .eqIfPresent(PlugOrderItemDO::getGiftGrowth, reqVO.getGiftGrowth())
+                .eqIfPresent(PlugOrderItemDO::getAppInfo, reqVO.getAppInfo())
+                .betweenIfPresent(PlugOrderItemDO::getCreateTime, reqVO.getCreateTime())
+                .orderByDesc(PlugOrderItemDO::getId));
+    }
+
+    default List<PlugOrderItemDO> selectList(PlugOrderItemExportReqVO reqVO) {
+        return selectList(new LambdaQueryWrapperX<PlugOrderItemDO>()
+                .eqIfPresent(PlugOrderItemDO::getOrderId, reqVO.getOrderId())
+                .eqIfPresent(PlugOrderItemDO::getAppId, reqVO.getAppId())
+                .eqIfPresent(PlugOrderItemDO::getAppPic, reqVO.getAppPic())
+                .likeIfPresent(PlugOrderItemDO::getAppName, reqVO.getAppName())
+                .eqIfPresent(PlugOrderItemDO::getAppSn, reqVO.getAppSn())
+                .eqIfPresent(PlugOrderItemDO::getAppPrice, reqVO.getAppPrice())
+                .eqIfPresent(PlugOrderItemDO::getPayNum, reqVO.getPayNum())
+                .eqIfPresent(PlugOrderItemDO::getPromotionAmount, reqVO.getPromotionAmount())
+                .eqIfPresent(PlugOrderItemDO::getDiscountAmount, reqVO.getDiscountAmount())
+                .eqIfPresent(PlugOrderItemDO::getGiftIntegration, reqVO.getGiftIntegration())
+                .eqIfPresent(PlugOrderItemDO::getGiftGrowth, reqVO.getGiftGrowth())
+                .eqIfPresent(PlugOrderItemDO::getAppInfo, reqVO.getAppInfo())
+                .betweenIfPresent(PlugOrderItemDO::getCreateTime, reqVO.getCreateTime())
+                .orderByDesc(PlugOrderItemDO::getId));
+    }
 
 }
