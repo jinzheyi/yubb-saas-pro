78e5a29c31057730532467aec4c6776ceeb5749f
zhusy <jin_zheyicn@qq.com>
2022-09-26 17:52:27 +0800

    生成插件商城代码

--- yudao-module-platform/yudao-module-platform-biz/src/main/java/cn/iocoder/yudao/module/platform/dal/mysql/plug/PlugGoodsMapper.java
+++ yudao-module-platform/yudao-module-platform-biz/src/main/java/cn/iocoder/yudao/module/platform/dal/mysql/plug/PlugGoodsMapper.java
@@ -5,9 +5,9 @@ import java.util.*;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
-import cn.iocoder.yudao.module.plug.dal.dataobject.goods.PlugGoodsDO;
+import cn.iocoder.yudao.module.platform.dal.dataobject.plug.PlugGoodsDO;
 import org.apache.ibatis.annotations.Mapper;
-import cn.iocoder.yudao.module.plug.controller.admin.goods.vo.*;
+import cn.iocoder.yudao.module.platform.controller.center.plug.vo.goods.*;
 
 /**
  * 应用商品 Mapper
