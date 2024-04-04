f80e945318b0d0e0b05e4caca3fb8ea2f3659913
jinzheyi <jin_zheyicn@qq.com>
2024-04-04 23:10:23 +0800

    钉钉改造0004-租户端登錄改造

--- yudao-ui/yudao-ui-admin-vue3/src/views/Login/components/MobileForm.vue
+++ yudao-ui/yudao-ui-admin-vue3/src/views/Login/components/MobileForm.vue
@@ -16,17 +16,6 @@
           <LoginFormTitle style="width: 100%" />
         </el-form-item>
       </el-col>
-      <el-col :span="24" style="padding-right: 10px; padding-left: 10px">
-        <el-form-item v-if="loginData.tenantEnable === 'true'" prop="tenantName">
-          <el-input
-            v-model="loginData.loginForm.tenantName"
-            :placeholder="t('login.tenantNamePlaceholder')"
-            :prefix-icon="iconHouse"
-            type="primary"
-            link
-          />
-        </el-form-item>
-      </el-col>
       <!-- 手机号 -->
       <el-col :span="24" style="padding-right: 10px; padding-left: 10px">
         <el-form-item prop="mobileNumber">
@@ -97,7 +86,7 @@ import type { RouteLocationNormalizedLoaded } from 'vue-router'
 
 import { useIcon } from '@/hooks/web/useIcon'
 
-import { setTenantId, setToken } from '@/utils/auth'
+import { setToken } from '@/utils/auth'
 import { usePermissionStore } from '@/store/modules/permission'
 import { getTenantIdByName, sendSmsCode, smsLogin } from '@/api/login'
 import LoginFormTitle from './LoginFormTitle.vue'
@@ -120,20 +109,17 @@ const { handleBackLogin, getLoginState } = useLoginState()
 const getShow = computed(() => unref(getLoginState) === LoginStateEnum.MOBILE)
 
 const rules = {
-  tenantName: [required],
   mobileNumber: [required],
   code: [required]
 }
 const loginData = reactive({
   codeImg: '',
-  tenantEnable: import.meta.env.VITE_APP_TENANT_ENABLE,
   token: '',
   loading: {
     signIn: false
   },
   loginForm: {
     uuid: '',
-    tenantName: '圣钰科技',
     mobileNumber: '',
     code: ''
   }
@@ -151,7 +137,6 @@ const smsVO = reactive({
 const mobileCodeTimer = ref(0)
 const redirect = ref<string>('')
 const getSmsCode = async () => {
-  await getTenantId()
   smsVO.smsCode.mobile = loginData.loginForm.mobileNumber
   await sendSmsCode(smsVO.smsCode).then(async () => {
     message.success(t('login.SmsSendMsg'))
@@ -174,16 +159,9 @@ watch(
     immediate: true
   }
 )
-// 获取租户 ID
-const getTenantId = async () => {
-  if (loginData.tenantEnable === 'true') {
-    const res = await getTenantIdByName(loginData.loginForm.tenantName)
-    setTenantId(res)
-  }
-}
+
 // 登录
 const signIn = async () => {
-  await getTenantId()
   const data = await validForm()
   if (!data) return
   ElLoading.service({
