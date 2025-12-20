11f1baebfbc0eda3622d6e5d7f663167238641a9
jin_zheyicn@qq.com <jin_zheyicn@qq.com>
2025-12-20 23:08:35 +0800

    im模块-重构19

---
+++ shengyu-ui/【H5和小程序】即时通讯/App.vue
@@ -0,0 +1,42 @@
+<script>
+	export default {
+		onLaunch: function() {
+			// #ifdef APP-PLUS-NVUE
+			// 加载公共图标库
+			const domModule = weex.requireModule('dom')
+			domModule.addRule('fontFace', {
+			    'fontFamily': "iconfont",
+			    'src': "url('https://at.alicdn.com/t/font_1365296_2ijcbdrmsg.ttf')"
+			});
+			// #endif
+			// #ifndef H5
+			// 初始化录音管理器
+			this.$store.commit('initRECORD')
+			// #endif
+			// 初始化登录状态
+			this.$store.dispatch('initLogin')
+			console.log('App Launch')
+		},
+		onShow: function() {
+			// this.$store.dispatch('reconnect')
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import "./common/free.css";
+	@import "./common/common.css";
+	/* #ifndef APP-PLUS-NVUE */
+	@import "./common/free-icon.css";
+	/* #endif */
+	/* #ifdef MP */
+	::-webkit-scrollbar{
+		display: none;
+	}
+	/* #endif */
+</style>
