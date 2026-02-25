0d9ede4327f60a06ebca9018751010d8201e8233
jin_zheyicn@qq.com <jin_zheyicn@qq.com>
2026-02-25 22:06:21 +0800

    2026-im-群文件实现

--- shengyu-ui/shengyu-ui-admin-vue3/src/api/infra/file/index.ts
+++ shengyu-ui/shengyu-ui-admin-vue3/src/api/infra/file/index.ts
@@ -49,5 +49,5 @@ export const createFile = (data: any) => {
 
 // 上传文件
 export const updateFile = (data: any, onUploadProgress?: Function) => {
-  return request.upload({ url: '/infra/file/upload', data, onUploadProgress })
+  return request.upload({ url: getUploadUrl() + '/infra/file/upload', data, onUploadProgress })
 }
